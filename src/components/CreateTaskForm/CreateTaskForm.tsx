b5f6612fba969d86d22b551cff854bca2d353aa8
LomovaAA <anastlomova14@gmail.com>
2021-10-28 13:01:37 +0300

    Форма добавления задачи реализована с помощью react-hook-form

--- src/components/CreateTaskForm/CreateTaskForm.tsx
+++ src/components/CreateTaskForm/CreateTaskForm.tsx
@@ -1,49 +1,54 @@
-import { Field, Form, Formik } from 'formik';
-import React from 'react';
-import { addTask } from '../../shared/store';
-import '../../shared/components/Modal/Modal.scss';
+import React from "react";
+import { useForm } from "react-hook-form";
+import styles from "../../shared/components/Modal/Modal.module.scss";
+import { SectionAlias } from "../../shared/enums/section-alias.enum";
+import { addTask } from "../../shared/store";
 
 export const CreateTaskForm: React.FC<{
-  sectionAlias: string;
+  sectionAlias: SectionAlias;
   onClose: () => void;
 }> = ({ sectionAlias, onClose }) => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (values: { title: string; content: string }) => {
+    addTask({
+      sectionAlias,
+      id: "",
+      title: values.title,
+      content: values.content,
+      creationDate: new Date(),
+      completed: false,
+    });
+    onClose();
+  };
+
   return (
-    <Formik
-      initialValues={{
-        title: '',
-        content: '',
-      }}
-      onSubmit={(values) => {
-        addTask({
-          sectionAlias,
-          id: '',
-          title: values.title,
-          content: values.content,
-          creationDate: new Date(),
-          completed: false,
-        });
-        onClose();
-      }}
-    >
-      <Form className="form">
-        <div className="form__item">
-          <label className="form__label" htmlFor="taskTitle">
-            Title
-          </label>
-          <Field className="form__input" id="taskTitle" name="title" />
-        </div>
-        <div className="form__item">
-          <label className="form__label" htmlFor="taskContent">
-            Text
-          </label>
-          <Field className="form__input" id="taskContent" name="content" />
-        </div>
-        <div>
-          <button className="form__submit-btn" type="submit">
-            Create
-          </button>
-        </div>
-      </Form>
-    </Formik>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.form__item}>
+        <label className={styles.form__label} htmlFor="taskTitle">
+          Title
+        </label>
+        <input
+          {...register("title", { required: true })}
+          className={styles.form__input}
+          id="taskTitle"
+          name="title"
+          type="text"
+        />
+      </div>
+      <div className={styles.form__item}>
+        <label className={styles.form__label} htmlFor="taskContent">
+          Text
+        </label>
+        <textarea
+          {...register("content", { required: true })}
+          className={styles.form__input}
+          id="taskContent"
+          name="content"
+        />
+      </div>
+      <button className={styles["form__submit-btn"]} type="submit">
+        Create
+      </button>
+    </form>
   );
 };
